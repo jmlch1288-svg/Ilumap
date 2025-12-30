9ca08b5dfa1b7395581b073c12f4b9569314391c
jmariol1042 <jmlch1288@gmail.com>
2025-12-30 16:29:30 -0500

    fix: agregar imágenes de logo y avatar user - header completo

--- frontend/src/components/auth/SignInForm.tsx
+++ frontend/src/components/auth/SignInForm.tsx
@@ -16,7 +16,7 @@ export default function SignInForm() {
           to="/"
           className="inline-flex items-center text-sm text-gray-500 transition-colors hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
         >
-          <ChevronLeftIcon className="size-5" />
+          <ChevronLeft className="size-5" />
           Back to dashboard
         </Link>
       </div>
@@ -105,9 +105,9 @@ export default function SignInForm() {
                       className="absolute z-30 -translate-y-1/2 cursor-pointer right-4 top-1/2"
                     >
                       {showPassword ? (
-                        <EyeIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
+                        <Eye className="fill-gray-500 dark:fill-gray-400 size-5" />
                       ) : (
-                        <EyeCloseIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
+                        <EyeOff className="fill-gray-500 dark:fill-gray-400 size-5" />
                       )}
                     </span>
                   </div>
